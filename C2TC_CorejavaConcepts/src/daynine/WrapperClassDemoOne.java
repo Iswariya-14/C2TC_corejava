e6a8ed72217aabdadbd44a3e5a2f91cc6c52c695
Iswariya-14 <iswariya1403@gmail.com>
2025-08-24 11:38:15 +0530

    commit

---
+++ C2TC_CorejavaConcepts/src/daynine/WrapperClassDemoOne.java
@@ -0,0 +1,20 @@
+//Program to demonstrate Command Line Arguments 
+package daynine;
+
+public class WrapperClassDemoOne {
+
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+	
+		if (args.length < 2) {
+	            System.out.println("Please provide two numbers as arguments.");
+	            return;
+	        }
+
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = a + b;
+		System.out.println(c);
+	}
+
+}
